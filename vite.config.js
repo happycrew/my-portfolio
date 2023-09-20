4a26d5036e7bf4ee7da5b0848b2b4766f8e2272d
Vladimir Orlov <satancrew.true@gmail.com>
2023-09-20 12:03:01 +0300

    feat: add dist

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url';
-import vitePluginRequire from 'vite-plugin-require';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
@@ -8,17 +7,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   base: '',
   assetDir: 'assets',
-  plugins: [
-    vue({
-      // This is needed, or else Vite will try to find image paths (which it wont be able to find because this will be called on the web, not directly)
-      // For example <img src="/images/logo.png"> will not work without the code below
-      template: {
-        transformAssetUrls: {
-          includeAbsolute: false
-        }
-      }
-    }),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
